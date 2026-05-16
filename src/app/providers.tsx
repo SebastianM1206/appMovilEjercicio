d051127bb44ecba9d509e32023e06bf0ad818a41
Alejandro Bravo Isajar <alejandrobravoisajar1@gmail.com>
2026-05-16 17:19:29 -0500

    feat: add firebase auth context and service

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,11 +1,12 @@
 import type { FC, ReactNode } from 'react';
+import { AuthProvider } from '../features/auth/authContext';
 
 type AppProvidersProps = {
   children: ReactNode;
 };
 
 const AppProviders: FC<AppProvidersProps> = ({ children }) => {
-  return <>{children}</>;
+  return <AuthProvider>{children}</AuthProvider>;
 };
 
 export default AppProviders;
